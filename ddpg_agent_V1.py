995ceded45b4b0a7734091bb4c96ed421714ebcb
Quertier <tonyquertier@gmail.com>
2019-02-22 19:16:31 +0100

    Update ddpg_agent_V1.py

--- ddpg_agent_V1.py
+++ ddpg_agent_V1.py
@@ -10,11 +10,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 1024 # minibatch size
-GAMMA = 0.99            # discount factor
+BATCH_SIZE = 128 # minibatch size
+GAMMA = 0.97            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-3         # learning rate of the actor 
-LR_CRITIC = 1e-3        # learning rate of the critic
+LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_CRITIC = 1e-4        # learning rate of the critic
 WEIGHT_DECAY = 0.0  # L2 weight decay
 
 EPSILON = 1.0 
